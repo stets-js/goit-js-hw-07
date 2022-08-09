a6b19959a72e460bd42485a26e8306bb4ef30693
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-09 19:14:21 +0300

    gallery modal task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//MARKUP
+
+function makingGalleryMarkup(obj) {
+  return obj
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}" rel="nofollow">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+const galleryMarkup = makingGalleryMarkup(galleryItems);
+gallery.innerHTML = galleryMarkup;
+
+// MODAL
+
+const onGalleryItemClick = function (event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show(() => {
+    document.body.addEventListener("keydown", (e) => {
+      if (e.code === "Escape") {
+        instance.close();
+      }
+      console.log(e);
+    });
+  });
+};
+
+gallery.addEventListener("click", onGalleryItemClick);
